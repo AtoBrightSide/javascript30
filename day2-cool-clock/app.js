c772eaba39f92a17b6546467c09f1c4790909e9d
Abel Mesele <meseleabel99@gmail.com>
2024-09-07 11:57:34 +0300

    Implement clock

---
+++ day2-cool-clock/app.js
@@ -0,0 +1,15 @@
+const secondHand = document.getElementById("second-hand");
+const minuteHand = document.getElementById("min-hand");
+const hourHand = document.getElementById("hour-hand");
+
+setInterval(() => {
+    let currSecond = new Date().getSeconds();
+    if (currSecond === 0) currSecond = 60;
+    secondHand.style.transform = `rotate(${(currSecond * 6) + 90}deg)`
+    let currMin = new Date().getMinutes();
+    if (currMin === 0) currMin = 60;
+    minuteHand.style.transform = `rotate(${(currMin * 6) + 90}deg)`
+    let currHour = new Date().getHours();
+    if (currHour === 0) currHour = 12;
+    hourHand.style.transform = `rotate(${(currHour * 30) + 90}deg)`
+}, 1000)
